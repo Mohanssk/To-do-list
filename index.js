2c987f385458cf1a6a73ba6a7c24e3c5dfc34e7f
mohanssk <mohankaruparthi7@gmail.com>
2025-12-14 02:10:32 +0530

    Intial commit

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ const port = process.env.PORT || 3000;
 
 const db = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
-  },
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
 });
 db.connect();
 
